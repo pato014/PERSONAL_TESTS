7b9edfab128c39f769e87e9825eac4a5bb2436b7
pato014 <nika.patatishvili@gmail.com>
2023-09-10 00:22:01 +0400

    simple web scrapping

---
+++ web_scrapping/main.py
@@ -0,0 +1,16 @@
+from operations import ops
+
+URL = 'http://programmer100.pythonanywhere.com/tours/'
+FILE_DIR = 'data.txt'
+
+
+
+if __name__ == "__main__":
+    scraped = ops.scrape(URL)
+    extracted = ops.extract(scraped)
+    print(extracted)
+
+    content = ops.read(FILE_DIR)
+    if extracted != "No upcoming tours" and extracted not in content:
+        ops.store(extracted, FILE_DIR)
+        ops.send_email()
